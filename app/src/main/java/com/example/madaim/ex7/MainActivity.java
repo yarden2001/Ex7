2250aa846bd6074265d45c37c7ce1c2b9ea6cf98
Madaim <yarden2001@hotmail.com>
2017-11-24 11:27:54 +0200

    newwwww

--- app/src/main/java/com/example/madaim/ex7/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex7/MainActivity.java
@@ -2,13 +2,49 @@
 package com.example.madaim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static com.example.madaim.ex7.R.layout.activity_main;
 
 public class MainActivity extends Activity {
+    final int REGISTER_CODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToActivity2 (View view){
+        Intent intent = new Intent (this, RegisterActivity.class);
+        startActivityForResult(intent,REGISTER_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REGISTER_CODE)
+        {
+            String gender;
+
+            if(data.getStringExtra("gender").compareTo("Male")==0){
+                gender = "Mr. ";
+            }
+            else{
+                gender = "Ms. ";
+            }
+            String returnString = "Welcome back "+gender+" "+data.getStringExtra("firstName")+", "+data.getStringExtra("lastName");
+            TextView textView = (TextView)findViewById(R.id.textView);
+            Button b = (Button) findViewById(R.id.button);
+            b.setText("again...");
+            textView.setText(returnString);
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/madaim/ex7/RegisterActivity.java
+++ app/src/main/java/com/example/madaim/ex7/RegisterActivity.java
@@ -1,7 +1,13 @@
 package com.example.madaim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class RegisterActivity extends Activity {
 
@@ -10,4 +16,24 @@ public class RegisterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    public void onButtonClick(View view) {
+
+
+        EditText firstName = (EditText) findViewById(R.id.editText);
+        EditText lastName = (EditText) findViewById(R.id.editText2);
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+        int gender = rg.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton) findViewById(gender);
+        String genderS = rb.getText().toString();
+        String stringToPassBack = firstName.getText().toString();
+        String stringToPassBack2 = lastName.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra("firstName", stringToPassBack);
+        intent.putExtra("lastName", stringToPassBack2);
+        intent.putExtra("gender", genderS);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
 }
+
